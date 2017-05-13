8078484ac4ed9f6fde511bf3d635ee7bf9d599d6
logonrm <logonrm@fiap.com.br>
2017-05-12 22:04:29 -0300

    Primeiro Exercicio RM78653

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,21 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
 
 
+var minhavariavel="teste"
+
+minhavariavel = 100;
+
+function validaCampo(meuForm){
+    // var meuForm = document.getElementById("form_01");
+
+    if(meuForm.iNUser.value == ""){
+        meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
+        return false;
+
+    }
+    else{
+        return true;
+    }
+}
+
+//minhavariavel = {"100","70","90"}
