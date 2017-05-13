d72beeec2b6129d18c80fbd7565f3ee6d269ed59
logonrm <logonrm@fiap.com.br>
2017-05-12 22:18:02 -0300

    PRIMEIRO EXERCICIO RM 77721

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -7,6 +7,9 @@ function validaCampo(meuForm){
   if(meuForm.iNUser.value == ""){
     meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
     return  false;
+  }else if (meuForm.iSenha.value == "") {
+    meuForm.iSenha.style = "box-shadow:1px 1px 20px #ff0000;";
+    return  false;
   }else{
     return true;
   }
