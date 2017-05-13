175a5b8da51cc7ea87860c3da895a021ba2f037e
logonrm <logonrm@fiap.com.br>
2017-05-12 22:06:40 -0300

    PRIMEIRO EXERCICIO RM79019

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,19 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
 
+//var minhaVariavel = "UM VALOR QUALQUER";
+//minhaVariavel = 100;
+//alert("Valor da variavel: " + minhaVariavel)
 
+function validaCampo(meuForm){
+  //var meuForm = document.getElementById("form_01");
+  if(meuForm.iNUser.value == ""){
+    meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
+    return false;
+  }else{
+    return true;}
+
+  }
+
+function campoNovo(campinho){
+  iNUser.style = "none;";
+}
