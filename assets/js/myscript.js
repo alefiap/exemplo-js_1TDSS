f0b31e54f49b890c9503e13b553cfa4b8d36ec59
logonrm <logonrm@fiap.com.br>
2017-05-12 22:02:53 -0300

    PRIMEIRO EXERCICIO 77246

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,17 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
 
+function validaCampo(meuForm){
+  // var meuForm = document.getElementById("form_01");
 
+  if (meuForm.iNUser.value == ""){
+    meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+//Exercicio 1
+function retornaCampo(meuForm) {
+  iNUser.style = "none;";
+}
