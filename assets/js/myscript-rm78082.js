8799cb719e989b6e1152158eb10d9a5fd5174f3f
logonrm <logonrm@fiap.com.br>
2017-05-12 22:09:12 -0300

    PRIMEIRO COMMIT EXERCICIO RM78082

---
+++ assets/js/myscript-rm78082.js
@@ -0,0 +1,15 @@
+/* document.write ("versão do navegador" + navigator.userAgent) */
+
+//var minhaVariavel = 100;
+//alert("Valor da Variável " + minhaVariavel);
+
+function validaCampo(meuForm) {
+  //var meuForm = document.getElementById("form_01");
+
+  if(meuForm.iNUser.value == "") {
+    meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
+    return false
+  } else {
+    return true;
+  }
+}
