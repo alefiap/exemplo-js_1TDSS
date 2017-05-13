ff8dcfb3542fd8403228fa425003d6fba0cbffb7
logonrm <logonrm@fiap.com.br>
2017-05-12 22:06:59 -0300

    PRIMEIRO EXERCICIO RM78288

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,23 @@
-/*document.write("Versão Navegador : " + navigator.userAgent);*/
+/*document.write("Versão Navegador : " + navigator.userAgent);
 
+var minhaVariavel = "UM VALOR QUALQUER";
 
+minhaVariavel = 100;
+
+alert("Valor da variavel: " + minhaVariavel);*/
+
+function validaCampo(meuForm) {
+
+  // var meuForm = document.getElementById("form_01");
+
+  if(meuForm.iNUser.value == "") {
+    meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function campoNormal() {
+  document.getElementById("iNUser").style = "box-shadow: none;";
+}
