db579dce89b9154e5f6959331c0e4c6aa1677a90
logonrm <logonrm@fiap.com.br>
2017-05-12 22:01:03 -0300

    PRIMEIRO EXERCICIO RM76654

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,22 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
+/*var variavel_01 = "TESTE"
+variavel_01 = 100;
+alert("Valor da variavel: " + variavel_01);*/
+
+function validaCampo(formulario) {
+//var formulario = document.getElementById('form_01');
+
+  if(formulario.iNUser.value ==""){
+    formulario.iNUser.style = "box-shadow:1px 1px 20px #ff0000; background-color: pink;";
+    return false;
+  }else {
+    return true;
+  }
 
 
+}
+
+function tiraFormatacao(formulario){
+  iNUser.style = "none;";
+  }
+}
