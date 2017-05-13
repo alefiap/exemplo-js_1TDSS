f165a7e54fa63b3cb3dcaa52958ace2451424adf
logonrm <logonrm@fiap.com.br>
2017-05-12 22:09:24 -0300

    PRIMEIRO EXERCICIO RM79406

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,19 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
 
 
+var minhaVariavel = "Um Valor Qualquer";
+
+minhaVariavel = 100;
+
+function validaCampo(meuForm) {
+
+//var meuForm = document.getElementsByID("form_01");
+
+ if (meuForm.iNUser.value =="") {
+   meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
+ return false;
+}
+ else {
+   return true;
+ }
+}
