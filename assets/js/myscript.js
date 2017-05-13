aec70c5e17171c229027b5124b55c4bc6069c8f0
logonrm <logonrm@fiap.com.br>
2017-05-12 22:20:08 -0300

    PRIMEIRO EXERCICIO RM78421

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,17 +1,28 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
-/*var minhaVariavel = "UM VALOR QUALQUER";*/
-/*int nr = 100;*/
-/*alert("Valor da Variável: " + minhaVariavel);*/
+
+//var minhaVariavel = "UM VALOR QUALQUER"
+
+//minhaVariavel = 100;
+
+//alert("Valor da variavel :" + minhaVariavel); /*alert = função interna do javascript*/
 
 function validaCampo (meuForm) {
 
-  //var meuForm = document.getElementById("Form_01");
+  //var meuForm = document.getElementById("form_01");
 
-    if (meuForm.iNUser.value == ""){
-      meuForm.iNUser.style = "box-shadow: 1px 1px 20px #ff000;"
-      return false;
-    } else{
-      return true;
-    }
+  if(meuForm.iNUser.value == "" ){
+    meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
+    return false;
+  } else if(meuForm.iSenha.value == "") {
+    meuForm.iSenha.style = "box-shadow:1px 1px 20px #ff0000;";
+    return false;
+  }
 
+  else {
+    return true;
+  }
+}
+
+function resetaCampo (idCampo){
+  idCampo.style = "box-shadow:none;";
 }
