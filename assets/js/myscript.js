764e030c103f17499cb889756f6e97f54af31ab5
logonrm <logonrm@fiap.com.br>
2017-05-12 22:08:20 -0300

    PRIMEIRO EXERCICIO RM79235

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,17 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
 
+function validaCampo(meuForm){
 
+  //var meuForm = document.getElementById("form_01");
+  if (meuForm.iNUser.value == "") {
+     meuForm.iNUser.style = "box-shadow: 1px 1px 20px #ff0000;";
+     return false;
+  }else{
+    return true;
+  }
+
+}
+
+function normalizarCampo(){
+  document.getElementById("iNUser").style = "box-shadow:none;";
+}
