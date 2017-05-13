3714538fbb49d5602a99a6832bb456713ff8bb8a
logonrm <logonrm@fiap.com.br>
2017-05-12 22:01:26 -0300

    PRIEMEIRO EXERCICIO RM78892

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,19 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
+var minhaVariavel= "Qualquer valor";
 
 
+function validaCampo(meuForme) {
+  //var meuForme=document.getElementById("form_01");
+
+    if(meuForme.iNUser.value == ""){
+      meuForme.iNUser.style="box-shadow:1px 1px 20px #ff0000;";
+      return false;
+    }else {
+      return true;
+    }
+}
+
+function voltaAoNormal(meuForme){
+    .meuForme.iNUser.style="box-shadow: none;";
+
+}
