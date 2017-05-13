f493bf4f4dab85ca913b3752425fdd98b313747b
logonrm <logonrm@fiap.com.br>
2017-05-12 22:02:50 -0300

    Primeiro Exercicio RM 77861

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,17 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
 
+function validarCampo(meuForm) {
+  //var meuForm = document.getElementById("form_01");
+  if(meuForm.iNUser.value == ""){
+    meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
+    return false;
+  }else{
+    return true;
+  }
+}
 
+function retornoCampo() {
+
+  iNUser.style = "none;";
+
+}
