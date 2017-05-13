3116d2b93df3f600ff0f9f0c18520320fa6834db
logonrm <logonrm@fiap.com.br>
2017-05-12 22:06:54 -0300

    PRIMEIRO EXERCICIO RM 77721

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,16 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
-
-
+//var minhaVariavel = "Bundinha";
+//minhaVariavel=100;
+//alert("valor variavel: " + minhaVariavel);
+function validaCampo(meuForm){
+//  var meuForm = document.getElementById('form_01');
+  if(meuForm.iNUser.value == ""){
+    meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
+    return  false;
+  }else{
+    return true;
+  }
+}
+function campo(tto){
+  iNUser.style = "none;";
+}
