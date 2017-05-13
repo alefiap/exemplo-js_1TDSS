c8c14c568f5a57da7d3d20cdeb0bd34de4a7483c
logonrm <logonrm@fiap.com.br>
2017-05-12 22:52:50 -0300

    Primeiro exercicio RM79001

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,24 @@
 /*document.write("Versão Navegador : " + navigator.userAgent);*/
+//var minhaVariavel = "Um valor qualquer";
+//minhaVariavel = 100;
+//alert("Valor da variável: " + minhaVariavel);
 
+function validaCampo(meuForm){
 
+  // var meuForm = document.getElementById("form_01");
+    if (meuForm.iNUser.value == "") {
+      meuForm.iNUser.style = "box-shadow:1px 1px 20px #ff0000;";
+      return false;
+    }
+    else if(meuForm.iSenha.value == ""){
+      meuForm.iSenha.style = "box-shadow:1px 1px 20px #ff0000;";
+      return false;
+    }
+    else{
+      return true;
+    }
+
+function campoEmPreenchimento(idCampo){
+  idCampo.style = "box-shadow:none;";
+  }
+}
